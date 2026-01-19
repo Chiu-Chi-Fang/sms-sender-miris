2a4a8cd86b6791eb2f6bc7856818586d27c845a7
MIRIS HOME <ckshdick996099@gmail.com>
2026-01-19 22:55:31 +0800

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,18 @@
+function getWeekday(date){return date.getDay()}
+
+
+function getThisWeek(date, target){
+const d=new Date(date)
+d.setDate(d.getDate() - d.getDay() + target)
+return d
+}
+
+
+function getNextWeek(date, target){
+const d=getThisWeek(date,target)
+d.setDate(d.getDate()+7)
+return d
+}
+
+
+function fmt(d){return d.toISOString().slice(0,10)}
